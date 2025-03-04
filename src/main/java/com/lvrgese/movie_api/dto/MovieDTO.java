e98197fc8c58732a47458317cceff310ae071c32
Liyons Varghese <liyonsvarghese@gmail.com>
2025-03-04 11:23:40 +0530

    File handling

---
+++ src/main/java/com/lvrgese/movie_api/dto/MovieDTO.java
@@ -0,0 +1,34 @@
+package com.lvrgese.movie_api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieDTO {
+    private Integer movieId;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Director name is required")
+    private String directorName;
+
+    @NotBlank(message = "Studio name is required")
+    private String studioName;
+
+    private Set<String> movieCast;
+
+    private Integer releaseYear;
+
+    @NotBlank(message = "poster is required")
+    private String poster;
+
+    @NotBlank(message = "poster is required")
+    private String posterUrl;
+}
